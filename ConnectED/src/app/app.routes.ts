531886d1c33c66fbe1d860244f6481227d6d5c8e
Edow Jiru <agiantleapproduction@gmail.com>
2026-04-27 16:15:05 -0500

    added routing in app.html, plus setup some routers for home components. Created basic post-cards for summary component

--- ConnectED/src/app/app.routes.ts
+++ ConnectED/src/app/app.routes.ts
@@ -1,3 +1,3 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { Dashboard } from './components/home-components/dashboard/dashboard';
+export const routes: Routes = [{ path: '', component: Dashboard }];
--- ConnectED/src/app/components/home-components/summary/summary.ts
+++ ConnectED/src/app/components/home-components/summary/summary.ts
@@ -6,4 +6,50 @@ import { Component } from '@angular/core';
   templateUrl: './summary.html',
   styleUrl: './summary.css',
 })
-export class Summary {}
+export class Summary {
+  posts = [
+    {
+      name: 'Student 1',
+      community: 'Community Name',
+      date: '27, April 2026',
+      role: 'Computer Science Major',
+      roleType: 'student', //controls pink vs yellow color
+      body: 'Computer Science assignment 5 took forever. But it eas good practice!',
+    },
+    {
+      name: 'Teacher 1',
+      community: 'Community Name',
+      date: '25, April 2026',
+      role: 'Computer Science Instructor',
+      roleType: 'teacher',
+      body: "Class, assignment 5 will be do two weeks from now. Start on it early I wouldn't wait last minute.",
+    },
+
+    {
+      name: 'Student 2',
+      community: 'Community Name',
+      date: '26, April 2026',
+      role: 'Software Engineer Major',
+      roleType: 'student',
+      body: 'Anyone taking database systems next semester?',
+    },
+
+    {
+      name: 'Teacher 1',
+      community: 'Community Name',
+      date: '25, April 2026',
+      role: 'Computer Science Instructor',
+      roleType: 'teacher',
+      body: "Class, assignment 5 will be do two weeks from now. Start on it early I wouldn't wait last minute.",
+    },
+
+    {
+      name: 'Student 1',
+      community: 'Community Name',
+      date: '27, April 2026',
+      role: 'Computer Science Major',
+      roleType: 'student', //controls pink vs yellow color
+      body: 'Computer Science assignment 5 took forever. But it eas good practice!',
+    },
+  ];
+}
